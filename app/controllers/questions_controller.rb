d7a742f334686302a2e972f4896cc57734b1a9ad
umekazu <thegardenofsinners123@gmail.com>
2019-07-20 07:59:40 +0900

    質問の編集、削除

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,6 +13,22 @@ class QuestionsController < ApplicationController
     redirect_to questions_path
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update(question_params)
+    redirect_to questions_path
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
+
   private
     def question_params
       params.require(:question).permit(:text)
